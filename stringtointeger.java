d95f278ea14dc5b16974c1892a7263e37386067e
megalakrishnan <megalakrishnan@users.noreply.github.com>
2016-09-09 16:54:24 +0530

    Create stringtointeger.java

---
+++ stringtointeger.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class simple
+{
+   public static void main(String args[])
+   {
+      Scanner sc = new Scanner(System.in);
+      String n = sc.nextLine();
+      int d=Integer.parseInt(n);
+      System.out.println(d);
+      
+   }
+}
